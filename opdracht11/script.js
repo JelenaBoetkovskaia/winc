b0f9f9737a8da48b029f0b54aec2862c4b584475
Jelena <89603801+JelenaBoetkovskaia@users.noreply.github.com>
2021-11-14 18:00:43 +0100

    Add files via upload

---
+++ opdracht11/script.js
@@ -0,0 +1,23 @@
+console.log("Hello Winc Academy")
+//Метод console.log() выводит отладочную информацию в консоль, т.е. скрывая ее от пользователей.
+//typeof console.log='function', typeof- reserv key word))
+let myName = 'Jelena';
+console.log(myName);
+//let if = 'error';
+//Uncaught SyntaxError: Unexpected identifier,if=reserv key word
+let num1 = 4;
+let num2 = 4;
+console.log(num1 + num2);
+//let num1 = '4';
+//let num2 = '4';
+//console.log(num1 + num2);
+//also possilble, but with another result))
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+let age = 51;
+console.log(age);
+let newAge = '51';
+console.log(newAge);
+
