2937c590c4e939470b449ec2c40a83221b21aff6
Jelena <89603801+JelenaBoetkovskaia@users.noreply.github.com>
2021-11-18 12:41:11 +0100

    Add files via upload

--- Opdracht 12/script.js
+++ Opdracht 12/script.js
@@ -1,1 +1,9 @@
-
+console.log("Hello Winc Academy students");
+/*
+let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);
+//3.This is a large sum.
+let division = 637263 / 54;//3.This is a large sum too.
+console.log("division:", division);
+*/
+//6.I can't see the result in the console because I use a block comment on multiple lines.
