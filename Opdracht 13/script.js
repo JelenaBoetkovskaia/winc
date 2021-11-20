c1114d07dc67cbc2df390886658d714911fabb12
Jelena <89603801+JelenaBoetkovskaia@users.noreply.github.com>
2021-11-20 15:25:03 +0100

    Add files via upload

--- Opdracht 13/script.js
+++ Opdracht 13/script.js
@@ -1,1 +1,45 @@
+const age = 28; //Consloe.log is number.
+console.log(typeof 88);
+const isFemale = true; //console.log is bulean.
+console.log(typeof true);
+const driverStatus = 'BOB'; //Conssole.log is string.
+console.log(typeof 'BOB');
+
+if (age > 18) {
+
+    document.write("Welcome on board!");
+
+}
+if (age == 18) {
+
+    document.write("You are allowed!");
+
+}
+if (age < 18) {
+
+    document.write("Try next year.");
+
+}
+if (isFemale) {
+
+    document.write("Welcome bitch!");
+
+} else {
+
+    document.write("Go home Motherfucker.");
+
+}
+if (driverStatus) {
+
+    document.write("Drive safe home!")
+    console.log(document.write); // Console uitput : ƒ write() { [native code] }
+
+} else {
+
+    document.write("Call the taxi!");
+    console.log(document.write); // Console uitput : ƒ write() { [native code] }
+
+}
+
+
 
