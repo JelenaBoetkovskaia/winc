dc68eb01acb567f81496e8a8863699a5a2bff9e4
Jelena <89603801+JelenaBoetkovskaia@users.noreply.github.com>
2021-11-27 15:50:59 +0100

    Add files via upload

--- Opdracht 16/script.js
+++ Opdracht 16/script.js
@@ -1,1 +1,15 @@
-
+const makePizza = function () {
+    console.log("Order Uber Eats");
+    console.log("Wait for courier");
+    console.log("Eat your pizza");
+}
+makePizza();
+makePizza();
+makePizza();
+const makeSushi = function () {
+    console.log("Order Uber Eats");
+    console.log("Wait for courier");
+    console.log("Eat your Sushi");
+}
+makeSushi();
+makeSushi();
