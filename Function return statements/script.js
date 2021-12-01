c144fb0680c32f64b20de2d286966cb2111322f1
Jelena <89603801+JelenaBoetkovskaia@users.noreply.github.com>
2021-12-01 19:47:50 +0100

    Add files via upload

--- Function return statements/script.js
+++ Function return statements/script.js
@@ -1,1 +1,44 @@
+//Checking if a number is big (functions that do something).
+
+const check = function (a) {
+    const number = a;
+    if (number <= 100) {
+        return false;
+    }
+    return true;
+};
+
+let b = check(100);
+console.log(b);
+let c = check(-100);
+console.log(c);
+let d = check(200);
+console.log(d);
+let e = check(20);
+console.log(e);
+
+//Bouncer at a club(functions that do something).
+const people = 50;
+const age = 20;
+function checkPeople() {
+    return people
+}
+function checkAge() {
+    return age
+}
+
+const output = `
+${people > 50 ? 'Welcome onb board 👍 and show your ID' : 'Its too busy now, come back later😒'}
+${age >= 21 ? 'You are in ' : 'This is a club for adults🤦‍♂️.'}
+`
+console.log(output)
+
+//Calculating the average (functions that produce something).
+
+
+function average(nums) {
+    return nums.reduce((a, b) => (a + b)) / nums.length;
+}
+
+const arr = [1,2,3,4,5];
 
