a79b9855771fa79bb8769343a419f123bb283c39
Jelena <89603801+JelenaBoetkovskaia@users.noreply.github.com>
2021-12-08 22:15:33 +0100

    Add files via upload

--- While loops & for loops/even-odd-reporter.js
+++ While loops & for loops/even-odd-reporter.js
@@ -1,1 +1,17 @@
+//1. Even Odd Reporter
+for (let i = 0; i <= 20; i++) {
+    console.log(`Is ${i} even or odd?`);
+    const checkFirst = (i) => {
+        return i % 2 > 0
+    };
+    //console.log(checkOdd(i)); true & false
+    const evenOdd = i => (checkFirst(i) ? "odd" : "even");
+
+    console.log(evenOdd(i)); //odd&even  0 is even
+};
+
+
+
+
+
 
