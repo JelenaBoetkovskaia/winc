fdd96e33ee6be258939b05bc8f0d3f0d6477dd2e
Jelena <89603801+JelenaBoetkovskaia@users.noreply.github.com>
2021-12-17 17:15:43 +0100

    Add files via upload

--- Exercise: use Eventhandlers (defer)/script.js
+++ Exercise: use Eventhandlers (defer)/script.js
@@ -1,1 +1,29 @@
+//Part 1: Add a click event to a button:
+//a)
+const but = document.querySelector('#mybutton')
+but
+// but.onclick = () => {
+//     alert("Button clicked")
+// };
 
+//b)
+but.addEventListener('click', () => {
+    //console.log("click")
+    alert("Button clicked too")
+});
+
+// Part 2:
+
+const button = document.querySelector('#mybutton2')
+const bodyColor = document.querySelectorAll('.blue-background')[0]
+button.addEventListener('click', () => {
+    //bodyColor.className = "blue-background red-background"
+    //or
+    //bodyColor.classList.add("red-background")
+});
+
+//Part 3:
+
+button.addEventListener('click', () => {
+    bodyColor.classList.toggle("red-background");
+});
