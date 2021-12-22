10e5e53db79225855c7929d9e66c3b548bd90daa
Jelena <89603801+JelenaBoetkovskaia@users.noreply.github.com>
2021-12-22 13:30:14 +0100

    Add files via upload

--- Final assignment: Color Toggle/script.js
+++ Final assignment: Color Toggle/script.js
@@ -1,1 +1,10 @@
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+menuBtn.addEventListener('mouseenter', function () {
+  menuBtn.classList.toggle('active');
+  menu.classList.toggle('active');
+})
 
+const bodyElement = document.body;
+//bodyElement.style.backgroundColor = 'blue';
+const getTag = document.getElementsByTagNameNS
